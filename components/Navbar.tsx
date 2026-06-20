8479689e91154824145cd4f753e3a48271aded5a
Xintong Zou <xintongzou.xz@gmail.com>
2026-06-21 04:34:29 +0800

    Update resume link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef, useState } from "react";
 const links = [
   { href: "/#work", label: "Project", scrollTo: "work" },
   { href: "/#about", label: "About", scrollTo: "about" },
-  { href: "https://drive.google.com/file/d/1eQo8PHiHhcs9STQnhwButIuo2aJ2b6W7/view?usp=drive_link", label: "Resume", external: true },
+  { href: "https://drive.google.com/file/d/1924WuV6Dt1wOkeWYqD7g3Mbh6cJFMudu/view?usp=sharing", label: "Resume", external: true },
 ];
 
 function scrollEaseOut(targetId: string) {
